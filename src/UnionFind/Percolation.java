141cc6a8d6eaa60cf807fbb53c319a5c9e0120e6
balag3 <carbalage@gmail.com>
2017-01-07 15:15:22 +0100

    Adds javadoc comments

--- src/UnionFind/Percolation.java
+++ src/UnionFind/Percolation.java
@@ -4,14 +4,39 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 import java.util.Arrays;
 
+
 public class Percolation {
+    /**
+     * The size of the grid's side
+     */
     private int n;
+    /**
+     * The square of the grid.
+     */
     private int size;
+    /**
+     * Represents the top row of the grid
+     */
     private int top;
+    /**
+     * Represents the bottom row of the grid
+     */
     private int bottom;
+    /**
+     * The grid as a two dimensional array
+     */
     private int[][] grid;
+    /**
+     * The Weighted Quick Union Find algorithm.
+     */
     private WeightedQuickUnionUF uf;
 
+
+    /**
+     * Initializes a new Percolation data structure.
+     *
+     * @param n the size of the grid (n by n) where n > 0
+     */
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException();
@@ -24,10 +49,21 @@ public class Percolation {
         this.uf = new WeightedQuickUnionUF(size + 2);
     }
 
+    /**
+     * A method to convert the two dimensional array coordinates to a single index integer.
+     * @param row The row coordinate of the grid.
+     * @param col The column coordinate of the grid
+     * @return The index number that represents the coordinates.
+     */
     private int coordToIndex(int row, int col) {
         return row * n + col;
     }
 
+    /**
+     * Method to validate input arguments.
+     * @param row The row coordinate of the block.
+     * @param col The column coordinate of the block.
+     */
     private void checkInput(int row, int col) {
         if (row > n || col > n) {
             throw new IndexOutOfBoundsException("n");
@@ -40,6 +76,12 @@ public class Percolation {
         }
     }
 
+    /**
+     * Checking and connecting the open neighbouring blocks to the currently opened block.
+     * Also connects every 0 and n - 1 row blocks to the top and bottom properties.
+     * @param row The row coordinate of the currently opened block.
+     * @param col The column coordinate of the currently opened block.
+     */
     private void connectOpenNeighbours(int row, int col) {
         int tempRow = row - 1;
         int tempCol = col - 1;
@@ -67,12 +109,23 @@ public class Percolation {
 
     }
 
+    /**
+     * The if the block is open connect it to the base block.
+     * @param row The row coordinate of neighbouring block.
+     * @param col The column coordinate of the neighbouring block.
+     * @param index The index of the base block.
+     */
     private void tryUnion(int row, int col, int index) {
         if (isOpen(row, col)) {
             uf.union(coordToIndex(row - 1, col - 1), index);
         }
     }
 
+    /**
+     * Open the block.
+     * @param row The row coordinate of the block.
+     * @param col The column coordinate of the block.
+     */
     public void open(int row, int col) {
         checkInput(row, col);
         int tempRow = row - 1;
@@ -82,6 +135,11 @@ public class Percolation {
 
     }
 
+    /**
+     * Check if the block is open.
+     * @param row The row coordinate of the block.
+     * @param col The column coordinate of the block.
+     */
     public boolean isOpen(int row, int col) {
         checkInput(row, col);
         int tempRow = row - 1;
@@ -89,11 +147,20 @@ public class Percolation {
         return grid[tempRow][tempCol] == 1;
     }
 
+    /**
+     * Check if the block is connected to the top row.
+     * @param row The row coordinate of the block.
+     * @param col The column coordinate of the block.
+     */
     public boolean isFull(int row, int col) {
         checkInput(row, col);
         return uf.connected(top, coordToIndex(row - 1, col - 1));
     }
 
+    /**
+     * Check if the system percolates, is there a route between top and bottom row.
+     * @return true if the system percolates else false.
+     */
     public boolean percolates() {
         return uf.connected(top, bottom);
     }
@@ -111,9 +178,9 @@ public class Percolation {
         Percolation p = new Percolation(6);
         p.open(1, 6);
         p.open(2, 6);
-        System.out.println(p.isOpen(1,6));
-        System.out.println(p.isFull(1,6));
-        System.out.println(p.isFull(2,6));
+        System.out.println(p.isOpen(1, 6));
+        System.out.println(p.isFull(1, 6));
+        System.out.println(p.isFull(2, 6));
         System.out.println(p.toString());
         System.out.println(p.uf.count());
     }
